3f8b70c1d860ebe3fd70e38b4cb611c67b8d3999
Greg Hayes <gthelight@gmail.com>
2020-05-21 19:50:29 -0500

    server setup test routes

--- diners/diner-router.js
+++ diners/diner-router.js
@@ -7,6 +7,9 @@ router.get("/", async (req, res, next) => {
   try {
     res.status(200).json({ message: "Welcome to the Diner route!" });
   } catch (err) {
+    res.status(500).json({
+      message: "Something went wrong",
+    });
     next(err);
   }
 });
